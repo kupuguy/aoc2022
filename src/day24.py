5dc85a2023895885a27f9e59ba0e031a35321738
Duncan Booth <kupuguy@gmail.com>
2022-12-24 09:45:49 +0000

    Day 24: cleaned up into a class

--- src/day24.py
+++ src/day24.py
@@ -15,6 +15,11 @@ EXPECTED_2 = None
 DATA = Path("input/data24.txt").read_text().splitlines()
 
 
+def parse(data: list[str]) -> list[list[str]]:
+    rows = [line[1:-1] for line in data[1:-1]]
+    return rows
+
+
 @dataclass(frozen=True, slots=True, eq=True)
 class Point2D:
     x: int
@@ -30,74 +35,76 @@ class Point2D:
         }
 
 
-def parse(data: list[str]) -> list[list[str]]:
-    rows = [line[1:-1] for line in data[1:-1]]
-    return rows
+@dataclass
+class Worker:
+    map: list[list[str]]
+    width: int
+    height: int
+    time: int = 0
 
+    def blocked(self, pos: Point2D) -> bool:
+        x, y = pos.x, pos.y
+        map, time, width, height = self.map, self.time, self.width, self.height
+        if x < 0 or y < 0:
+            return not (x == 0 and y == -1)
+        if x >= width or y >= height:
+            return not (x == width - 1 and y == height)
 
-def blocked(
-    pos: Point2D, time: int, map: list[list[str]], width: int, height: int
-) -> bool:
-    x, y = pos.x, pos.y
-    if x < 0 or y < 0:
-        return not (x == 0 and y == -1)
-    if x >= width or y >= height:
-        return not (x == width - 1 and y == height)
-
-    return (
-        map[y][(x + time) % width] == "<"
-        or map[y][(x - time) % width] == ">"
-        or map[(y + time) % height][x] == "^"
-        or map[(y - time) % height][x] == "v"
-    )
-
-
-def options(
-    state: set[Point2D], time: int, map: list[list[str]], width: int, height: int
-) -> set[Point2D]:
-    new_state = set()
-    xmax, ymax = width - 1, height - 1
-    for p in state:
-        new_state |= {q for q in p.next() if not blocked(q, time, map, width, height)}
-    return new_state
-
-
-def char(
-    pos: Point2D, state, time: int, map: list[list[str]], width: int, height: int
-) -> bool:
-    x, y = pos.x, pos.y
-    if x < 0 or y < 0:
-        snow = "" if (x == 0 and y == -1) else "#"
-    elif x >= width or y >= height:
-        snow = "" if (x == width - 1 and y == height) else "#"
-    else:
-        snow = "".join(
-            [
-                "<" if map[y][(x + time) % width] == "<" else "",
-                ">" if map[y][(x - time) % width] == ">" else "",
-                "^" if map[(y + time) % height][x] == "^" else "",
-                "v" if map[(y - time) % height][x] == "v" else "",
-            ]
+        return (
+            map[y][(x + time) % width] == "<"
+            or map[y][(x - time) % width] == ">"
+            or map[(y + time) % height][x] == "^"
+            or map[(y - time) % height][x] == "v"
         )
-    if pos in state:
-        return "E" if len(snow) == 0 else "!"
 
-    return "." if len(snow) == 0 else snow[0] if len(snow) == 1 else str(len(snow))
+    def options(
+        self,
+        state: set[Point2D],
+    ) -> set[Point2D]:
+        self.time += 1
+        return {q for p in state for q in p.next() if not self.blocked(q)}
 
-
-def show(state, time, map, width, height) -> None:
-    print("Initial state" if time == 0 else f"Minute {time}")
-    for y in range(-1, height + 1):
-        print(
-            "".join(
-                char(Point2D(x, y), state, time, map, width, height)
-                for x in range(-1, width + 1)
+    def char(
+        self,
+        pos: Point2D,
+        state,
+    ) -> bool:
+        x, y = pos.x, pos.y
+        if x < 0 or y < 0:
+            snow = "" if (x == 0 and y == -1) else "#"
+        elif x >= self.width or y >= self.height:
+            snow = "" if (x == self.width - 1 and y == self.height) else "#"
+        else:
+            map, time, width, height = self.map, self.time, self.width, self.height
+            snow = "".join(
+                [
+                    "<" if map[y][(x + time) % width] == "<" else "",
+                    ">" if map[y][(x - time) % width] == ">" else "",
+                    "^" if map[(y + time) % height][x] == "^" else "",
+                    "v" if map[(y - time) % height][x] == "v" else "",
+                ]
             )
-        )
-    print()
+        if pos in state:
+            return "E" if len(snow) == 0 else "!"
+
+        return "." if len(snow) == 0 else snow[0] if len(snow) == 1 else str(len(snow))
+
+    def show(self, state) -> None:
+        print("Initial state" if self.time == 0 else f"Minute {self.time}")
+        for y in range(-1, self.height + 1):
+            print(
+                "".join(
+                    self.char(
+                        Point2D(x, y),
+                        state,
+                    )
+                    for x in range(-1, self.width + 1)
+                )
+            )
+        print()
 
 
-def score_1(data: list[str]) -> int:
+def score_1(data: list[str], debug: bool = False) -> int:
     map = parse(data)
     width = len(map[0])
     height = len(map)
@@ -106,16 +113,17 @@ def score_1(data: list[str]) -> int:
     end = Point2D(width - 1, height)
 
     state = {start}
-    time = 0
-    # show(state, time, map, width, height)
+    worker = Worker(map, width, height)
+    if debug:
+        worker.show(state)
     while end not in state:
-        time += 1
-        state = options(state, time, map, width, height)
-        # show(state, time, map, width, height)
-    return time
+        state = worker.options(state)
+        if debug:
+            worker.show(state)
+    return worker.time
 
 
-test_score = score_1(TEST)
+test_score = score_1(TEST, debug=True)
 print("test", test_score)
 assert test_score == EXPECTED_1
 print("part 1", score_1(DATA))
@@ -132,20 +140,17 @@ def score_2(data: list[str]) -> int:
     end = Point2D(width - 1, height)
 
     state = {start}
-    time = 0
+    worker = Worker(map, width, height)
 
     while end not in state:
-        time += 1
-        state = options(state, time, map, width, height)
+        state = worker.options(state)
     state = {end}
     while start not in state:
-        time += 1
-        state = options(state, time, map, width, height)
+        state = worker.options(state)
     state = {start}
     while end not in state:
-        time += 1
-        state = options(state, time, map, width, height)
-    return time
+        state = worker.options(state)
+    return worker.time
 
 
 if EXPECTED_2 is not None:
